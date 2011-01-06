95b531f3b13bff47fc8f3bc2492303a45c2681ac
Otavio Fernandes <otavio@neton.com.br>
2011-01-06 15:35:48 -0200

    start implementation of Router

--- index.php
+++ index.php
@@ -1,10 +1,17 @@
 <?php
 
-require_once __DIR__.'/app/lib/Direct/ClassLoader.php';
+require_once __DIR__.'/lib/Direct/ClassLoader.php';
 
 use Direct\ClassLoader;
 
 $loader = new ClassLoader();
+
+// register the namespaces to exposes to ClassLoader
 $loader->registerNamespaces(array(
     'Direct' => __DIR__.'/lib'
 ));
+$loader->register();
+
+use Direct\Router;
+$router = new Router();
+$router->test();
---
+++ lib/Direct/Router.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Direct;
+/**
+ * Router that route the client's request.
+ * 
+ * @author Otavio Fernandes <otavio@neton.com.br>
+ */
+class Router
+{
+    /**
+     * 
+     */
+    public function router()
+    {
+        echo "Teste";
+    }
+}
